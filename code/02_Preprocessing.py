7d47348bcd069276012b03e1f6f76dc7b4c611d6
Shrisha <shrishashegde02@gmail.com>
2022-05-03 23:51:07 -0700

    creating preprocessed csv file and improvements in preprocess_ingredients function

--- code/02_Preprocessing.py
+++ code/02_Preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 import math  
 
+
 def clean_data(df):
     df.loc[df['Label'] == "moisturizing-cream-oils-mists", 'Label'] = "moisturizer"
     df.loc[df['Label'] == "facial-treatments", 'Label'] = "face_treatment"
@@ -13,8 +14,9 @@ def clean_data(df):
 
 def preprocess_ingredients(df):
     df.dropna(subset=['ingredients'], inplace=True)
-    processed_ingredient = [t.split('\n') for t in df['ingredients']]
-    pattern = ['\r\n', 'Please', 'No Info', 'This product', 'Visit']
+    df.reset_index(drop=True, inplace=True)
+    processed_ingredient = [t.split('\r\n\r\n') for t in df['ingredients']]
+    pattern = ['\r\n', '-\w+:', 'Please', 'No Info', 'This product', 'Visit']
 
     for i in range(len(df)):
         num = len(processed_ingredient[i])
@@ -22,6 +24,7 @@ def preprocess_ingredients(df):
             if all(x not in processed_ingredient[i][j] for x in pattern):
                 df['ingredients'][i] = processed_ingredient[i][j]
     return df
+
 
 def skin_type_preprocessing(data):
     data['skin_type'].fillna(data.mode()['skin_type'][0], inplace=True)
@@ -40,15 +43,17 @@ def skin_type_preprocessing(data):
     data = data.join(pd.DataFrame.sparse.from_spmatrix(mlb.fit_transform(data.pop('skin_type')), index=data.index, columns=mlb.classes_))
     return data
 
+
 def preprocess_price(data):
     data['price'].fillna('$0', inplace=True)
-    for idx in data.iterrows():
+    for idx, row in data.iterrows():
         curr = str(data.loc[idx, 'price'])
         if ' ' in curr:
             data.loc[idx, 'price'] = int(math.ceil(float(curr.split(' ')[0][1:])))
         else:
             data.loc[idx, 'price'] = int(math.ceil(float(curr[1:])))
     return data
+
 
 def preprocess_rank(data):
     data.dropna(subset=['rank'], inplace=True)
@@ -57,6 +62,7 @@ def preprocess_rank(data):
         data.loc[idx, 'rank'] = float(curr.split(' ')[0])
     return data    
 
+
 if __name__ == '__main__':
     df = pd.read_csv('data/cosmetic.csv', na_values={'NA', '#NAME?'})
     clean_data(df)
@@ -64,3 +70,4 @@ if __name__ == '__main__':
     df = skin_type_preprocessing(df)
     df = preprocess_price(df)
     df = preprocess_rank(df)
+    df.to_csv('data/cosmetic_preprocess.csv', index=False)
